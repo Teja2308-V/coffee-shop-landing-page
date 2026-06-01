8d483f3d12eead47ed0d72e62020e1cee71f984d
Teja2308-V <tejaswinivajralu2308@gmail.com>
2026-06-01 13:38:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Coffee Shop Website Loaded");
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+
+        if(position < window.innerHeight - 100){
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
